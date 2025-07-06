3c7de0fe29e0da112830928a12c822d988adb6f4
이경준 <mjckoq@naver.com>
2025-07-06 22:27:39 +0900

    백준 2557번 Hello World 출력

---
+++ src/back0706_0712/q_2557.java
@@ -0,0 +1,16 @@
+package back0706_0712;
+
+public class q_2557 {
+
+    /**
+     * Hello World 출력하기
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+
+}
