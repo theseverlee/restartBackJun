fdaed2ba3b16ac9aa969d505459a99d19e420bd9
이경준 <mjckoq@naver.com>
2025-07-06 23:16:11 +0900

    백준 1001번 A-B

---
+++ src/back0706_0712/q_1001.java
@@ -0,0 +1,40 @@
+package back0706_0712;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class q_1001 {
+
+    /**
+     * A - B 사칙연산 2번째
+     * @param args
+     */
+
+    /**
+     * 값을 입력 받고 해당값을 연산
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        String num = s.nextLine();
+        String[] arr = num.split(" ");
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr.length-1 == i){
+                result -= Integer.parseInt(arr[i]);
+            } else {
+                result += Integer.parseInt(arr[i]);
+            }
+        }
+        System.out.println(result);
+
+
+
+
+    }
+
+}
