a17925f5731c2ba45d9fe6f7707bae28eed2e305
이경준 <mjckoq@naver.com>
2025-07-06 23:30:45 +0900

    백준 10998번 A*B

---
+++ src/back0706_0712/q_10998.java
@@ -0,0 +1,18 @@
+package back0706_0712;
+
+import java.util.Scanner;
+
+public class q_10998 {
+
+    /**
+     * 10098번 a x b 출력하라
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt() * s.nextInt());
+
+    }
+
+}
