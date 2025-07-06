43dc400fa3a14d0b78ff57e76b3385d824833a30
이경준 <mjckoq@naver.com>
2025-07-06 22:53:44 +0900

    백준 1000번 A+B

---
+++ src/back0706_0712/q_1000.java
@@ -0,0 +1,40 @@
+package back0706_0712;
+
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner;
+
+public class q_1000 {
+
+    /**
+     * A+B
+     * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+     */
+
+    /**
+     * 값을 입력 받아야 하고 해당값을 꺼내야함
+     */
+
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt(); // A
+        int b = s.nextInt(); // B
+
+//        System.out.println(a);
+//        System.out.println(b);
+        System.out.println(a + b);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
