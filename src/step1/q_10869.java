d753ed8e99102059f76003578f5205a4a358bf74
이경준 <mjckoq@naver.com>
2025-07-07 23:21:13 +0900

    백준 10869번 사칙연산

---
+++ src/step1/q_10869.java
@@ -0,0 +1,29 @@
+package step1;
+
+import java.util.Scanner;
+
+public class q_10869 {
+
+    /**
+     * 10869  주어진 두수를 사칙연산 하라
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+
+
+
+    }
+
+}
