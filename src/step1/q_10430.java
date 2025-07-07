0f51361fc97a223c97ee95e6b5e0323ba0db45a0
이경준 <mjckoq@naver.com>
2025-07-08 00:04:39 +0900

    백준 10430번 나머지 연산

---
+++ src/step1/q_10430.java
@@ -0,0 +1,44 @@
+package step1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class q_10430 {
+
+    /**
+     * (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+     *
+     * (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+     *
+     * 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedReader br = new BufferedReader(isr);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+//        boolean flag1 = (A+B)%C == ((A%C) + (B%C))%C;
+//        boolean flag2 = (A*B)%C == ((A%C) * (B%C))%C;
+
+        bw.write(String.valueOf((A+B)%C) +"\n");
+        bw.write(String.valueOf(((A%C) + (B%C))%C) +"\n");
+        bw.write(String.valueOf((A*B)%C) +"\n");
+        bw.write(String.valueOf(((A%C) * (B%C))%C));
+
+        bw.close();
+        br.close();
+        osw.close();
+        isr.close();
+
+    }
+
+
+}
