e6faf9b49586f7079d6ab255edd8327554bb84e4
이경준 <thesever2@gmail.com>
2025-07-10 00:04:58 +0900

    백준 10171번 고양이

---
+++ src/step1/q_10171.java
@@ -0,0 +1,21 @@
+package step1;
+
+public class q_10171 {
+
+    /**
+     * 고양이 만들기
+     * \    /\
+     *  )  ( ')
+     * (  /  )
+     *  \(__)|
+     */
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+
+}
